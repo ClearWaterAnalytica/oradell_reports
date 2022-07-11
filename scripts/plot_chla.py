Fix cell averaging over dates with missing satellite data.

scripts/plot_chla.py
@@ -54,7 +54,9 @@ lats = data["lat"]
 df = data.copy()
 for i in np.arange(-X,-1):
     df1 = pd.read_csv(files[i],parse_dates=["date"])
-    df["Chlorophyll"] = (df["Chlorophyll"] + griddata((df1["lon"],df1["lat"]), df1["Chlorophyll"],(lons, lats), method='nearest'))
+    oldChl = df["Chlorophyll"].to_numpy().reshape((-1,1))
+    newChl = griddata((df1["lon"],df1["lat"]), df1["Chlorophyll"],(lons, lats), method='nearest').reshape((-1,1))
+    df["Chlorophyll"] = np.nansum(np.hstack([oldChl, newChl]), 1)
 
 
 

scripts/plot_cyan.py
@@ -54,7 +54,9 @@ lats = data["lat"]
 df = data.copy()
 for i in np.arange(-X,-1):
     df1 = pd.read_csv(files[i],parse_dates=["date"])
-    df["log_CI_cells_mL"] = (df["log_CI_cells_mL"] + griddata((df1["lon"],df1["lat"]), df1["log_CI_cells_mL"],(lons, lats), method='nearest')) / 2
+    oldCyan = df["log_CI_cells_mL"].to_numpy().reshape((-1,1))
+    newCyan = griddata((df1["lon"],df1["lat"]), df1["log_CI_cells_mL"],(lons, lats), method='nearest').reshape((-1,1))
+    df["log_CI_cells_mL"] = np.nansum(np.hstack([oldCyan, newCyan]), 1) / 2
 
 
 